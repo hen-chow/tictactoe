fd7220699a20ca6eb62a46e0c828d63b1b710f13
Hen <Hen@HENRIETTAs-MacBook-Air.local>
2017-02-28 16:22:56 +1100

    Added JS functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
-window.ticTacToe = {
-
-    turnsCounter: 0,
+var ticTacToe = {
 
     board: [
       [null, null, null],
@@ -10,40 +8,42 @@ window.ticTacToe = {
       [null, null, null]
     ],
 
-    player1: {     
-      turns: ""
-    },
-
-    player2: {
-      turns: ""
-    },
+    player: 0,
+    turnsCounter: 0,
 
     recordEntries: function(player, x, y) {
       board = ticTacToe.board;
       board[x][y] = player;
-
       return board;
+    },
+
+    boardCheck: function(x,y) {
+      board = ticTacToe.board;
+      if (board[x][y] === null) {
+        return true;
+      }
+      return false;
     },
 
     winnerCheck: function () {
       board = ticTacToe.board;
       for (var i = 0; i < board.length; i++) {
-        if(board[i][0] === board[i][1] && board[i][0] === board[i][2]){
-          console.log(board[i]);
-          return "player " + board[i][0] + " wins";
+        if(board[i][0] != null && board[i][0] === board[i][1] && board[i][0] === board[i][2]){
+          console.log(board[i][0], board[i][1], board[i][2]);
+          return true;
         }
-        if(board[0][i] === board[1][i] && board[0][i] === board[2][i]){
+        if(board[0][i] != null && board[0][i] === board[1][i] && board[0][i] === board[2][i]){
           console.log(board[0][i], board[1][i], board[2][i]);
-          return "player " + board[0][i] + " wins";
+          return true;
         }
       }
-      if(board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+      if(board[0][0] != null && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
           console.log(board[0][0], board[1][1], board[2][2]);
-          return "player " + board[0][0] + " wins";
+          return true;
         }
-      if(board[0][2] === board[1][1] && board[0][2] === board[2][0]){
+      if(board [0][2] != null && board[0][2] === board[1][1] && board[0][2] === board[2][0]){
           console.log(board[0][2], board[1][1], board[2][0]);
-          return "player " + board[0][2] + " wins";
+          return true;
         }
 
       return false;
@@ -51,49 +51,94 @@ window.ticTacToe = {
     },
 
     swapTurns: function () {
-      var turns = 0;
+      ticTacToe.player = 1 - ticTacToe.player;
 
-
-
-
-
+      return this.player;
     }
 
-
   };
+
+  var addToken = function (square, token) {
+      $(square).addClass(token);
+  };
+
+  var resetBoard = function() {
+    $("#board td").removeClass("token-one token-two");
+    $("#update-container").html("");
+  }
+
+
+  $("#board td").on("click", function() {
+
+    var row = parseInt($(this).attr("row"));
+    var col = parseInt($(this).attr("col"));
+
+    if (ticTacToe.player === 0) {
+
+      if (ticTacToe.boardCheck(row, col) === true) {
+
+        ticTacToe.recordEntries(ticTacToe.player, row, col);
+
+        ticTacToe.turnsCounter += 1;
+        console.log(ticTacToe.turnsCounter);
+
+        addToken(this, "token-one");
+
+        if (ticTacToe.winnerCheck() === true) {
+          $("#update-container").css("color", "red").html("<h3>Player 1 is the winner!</h3>");
+
+          setTimeout(resetBoard, 5000);
+
+        } else if (ticTacToe.turnsCounter === 9){
+
+            $("#update-container").html("<h3>Game Over! It's a draw. Play again?</h3>");
+
+            setTimeout(resetBoard, 5000);
+
+        } else {
+
+          ticTacToe.swapTurns();
+
+          $("#update-container").html("<h3>It's Player 2's turn!</h3>");
+
+        };
+
+      } else {
+
+        $("#update-container").html("<h3>Spot taken. Pick another spot on the board.</h3>")
+
+      };
+
+    } else {
+
+      if (ticTacToe.boardCheck(row, col) === true) {
+
+        ticTacToe.recordEntries(ticTacToe.player, row, col);
+
+        ticTacToe.turnsCounter += 1;
+
+        addToken(this, "token-two");
+
+        if (ticTacToe.winnerCheck() === true) {
+          $("#update-container").css("color", "red").html("<h3>Player 2 is the winner!</h3>");
+
+          setTimeout(resetBoard, 5000);
+
+        } else {
+
+          ticTacToe.swapTurns();
+
+          $("#update-container").html("<h3>It's Player 1's turn!</h3>");
+
+        };
+
+      } else {
+
+        $("#update-container").html("<h3>Spot taken. Pick another spot on the board.</h3>")
+
+      }
+    };
+
+  })
+
 });
-//eventHandlers
-
-//Functions
-//Swapping turns
-//Taking a turn = clicks on space and icon updated on board
-// --> Icon update on board
-//Winner check
-//keep track of turns
-//keep track of where on board
-
-//1. Player1 takes turn no. 1
-//2. Player2 takes turn no. 1
-//3. Player1 takes turn no. 2
-//4. Player2 takes turn no. 2
-//5. Player1 takes turn no. 3
-//6. Winner check
-//7. If no winner, Player2 takes turn no. 3;
-//8. Winner check
-//9. If no winner, Player1 takes turn no. 4;
-//10. Winner check
-//11. If no winner, Player2 takes turn no. 4;
-//12. Winner check
-//13. If no winner, Player1 takes turn no. 5;
-//14. Winner check;
-//15. Declare winner or draw
-
-
-
-
-
-
-//
-
-//html for icon1 <i class="fa fa-circle-o" aria-hidden="true"></i>
-//html for icon2 <i class="fa fa-times" aria-hidden="true"></i>
