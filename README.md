5dab9ee88f4c2d6472743a47ed95fdada1a0db0d
Hen <Hen@192-168-1-30.tpgi.com.au>
2017-03-02 21:16:41 +1100

    added game counter and scoreboard function

---
+++ README.md
@@ -0,0 +1,16 @@
+#Title: Tic Tac Toe App
+
+###Type:
+- Project0
+
+###Summary:
+- Two player game
+
+###Objectives:
+- Win the game by strategically selecting where to place their marks.
+- The player who succeeds in placing three of their marks in a horizontal, vertical or diagonal row wins the game.
+
+###Instructions:
+- Each player to select icons before commencing the game. If neither players select icons, default icons will be assigned to them.
+- Once icons have been selected, each player will take turns to select a box until a player either wins or the game reaches a draw at the end of the 9th turn.
+- Scoreboard under the game board will keep track of game rounds.
--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ $(document).ready(function(){
     currentPlayerToken: 0,
     player0Token: "token-one",
     player1Token: "token-two",
+    gamesCounter: 0,
+    player1Wins: 0,
+    player2Wins:0,
 
     recordEntries: function(player, x, y) {
       board = ticTacToe.board;
@@ -32,20 +35,16 @@ $(document).ready(function(){
       board = ticTacToe.board;
       for (var i = 0; i < board.length; i++) {
         if(board[i][0] != null && board[i][0] === board[i][1] && board[i][0] === board[i][2]){
-          console.log(board[i][0], board[i][1], board[i][2]);
           return true;
         }
         if(board[0][i] != null && board[0][i] === board[1][i] && board[0][i] === board[2][i]){
-          console.log(board[0][i], board[1][i], board[2][i]);
           return true;
         }
       }
       if(board[0][0] != null && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
-          console.log(board[0][0], board[1][1], board[2][2]);
           return true;
         }
       if(board [0][2] != null && board[0][2] === board[1][1] && board[0][2] === board[2][0]){
-          console.log(board[0][2], board[1][1], board[2][0]);
           return true;
         }
 
@@ -56,7 +55,6 @@ $(document).ready(function(){
     swapTurns: function () {
       ticTacToe.player = 1 - ticTacToe.player;
       ticTacToe.currentPlayerToken = 1 - ticTacToe.currentPlayerToken;
-
       return this.player, this.currentPlayerToken;
     }
   };
@@ -83,22 +81,39 @@ $(document).ready(function(){
     $("#board td").removeClass("token-one token-two token-three token-four");
     $("#update-container").html("");
     $(".token").css("opacity", "1");
+    ticTacToe.player = 0;
+    ticTacToe.turnsCounter = 0;
+    ticTacToe.currentPlayerToken = 0;
+    ticTacToe.player0Token = "token-one";
+    ticTacToe.player1Token = "token-two";
 
     for (var i = 0; i < ticTacToe.board.length; i++){
-
       for (var j = 0; j <ticTacToe.board[i].length; j++){
-
         ticTacToe.board[i][j] = null;
       }
     }
   };
 
+  var winCounter = function() {
+    var player = ticTacToe.player;
+
+    if (player === 0) {
+      ticTacToe.player1Wins ++;
+    } else {
+      ticTacToe.player2Wins ++;
+    }
+  };
+
+  var scoreboardUpdate = function() {
+    $("#scoreboard").css("display", "inline").html("<h3>Round " + ticTacToe.gamesCounter + " </h3>" + "<p>Player 1: " + ticTacToe.player1Wins + " - " + ticTacToe.player2Wins + " : Player 2</p>");
+  }
+
   $("#tokens td").on("click", function() {
     var token = $(this).attr("class").split(" ").pop();
+    var player = ticTacToe.player;
 
-    if (ticTacToe.player === 0) {
+    if (player === 0) {
       ticTacToe.player0Token = token;
-      console.log(ticTacToe.player0Token);
       ticTacToe.player = 1 - ticTacToe.player;
       $(".token." + token).css("opacity", "0.5");
       $("#token-container h3").text("Player " + (ticTacToe.player + 1)+ ", select your icon.");
@@ -113,9 +128,8 @@ $(document).ready(function(){
     }
 
   });
-
+//board playing event handler
   $("#board td").on("click", function() {
-
     var row = parseInt($(this).attr("row"));
     var col = parseInt($(this).attr("col"));
     var square = $(this);
@@ -125,77 +139,140 @@ $(document).ready(function(){
   });
 
   var makeMove = function(row, col, square){
-
     if (ticTacToe.boardCheck(row, col) === true) {
-
       ticTacToe.recordEntries(ticTacToe.player, row, col);
-
       ticTacToe.turnsCounter += 1;
-
       addToken(square, ticTacToe.currentPlayerToken);
 
       if (ticTacToe.winnerCheck() === true) {
+        ticTacToe.gamesCounter +=1;
+        winCounter();
 
         setTimeout(function () {
-          $("#update-container").css("color", "red").html("<h3>Player " + (ticTacToe.player + 1) + " is the winner!</h3>");
+          $("#update-container").html("<h3>Player " + (ticTacToe.player + 1) + " is the winner!</h3>");
         }, 400);
-
         setTimeout(reset, 4000);
+        scoreboardUpdate();
 
       } else if (ticTacToe.turnsCounter === 9){
-
+        ticTacToe.gamesCounter +=1;
         setTimeout(function() {
           $("#update-container").html("<h3>Game Over! It's a draw. Play again?</h3>");
         }, 400);
-
         setTimeout(reset, 4000);
 
       } else {
-
         ticTacToe.swapTurns();
-
         setTimeout(function () {
           $("#update-container").html("<h3>It's Player " + (ticTacToe.player + 1) + "'s turn!</h3>");
         }, 400);
-
       };
 
     } else {
-
       $("#update-container").html("<h3>Spot taken. Pick another spot on the board.</h3>")
-
     };
   }
+//AI version
+  // var makeMove = function(row, col, square){
+  //   if (ticTacToe.boardCheck(row, col) === true) {
+  //     ticTacToe.recordEntries(ticTacToe.player, row, col);
+  //     ticTacToe.turnsCounter += 1;
+  //     addToken(square, ticTacToe.currentPlayerToken);
+  //
+  //     if (ticTacToe.winnerCheck() === true) {
+  //       ticTacToe.gamesCounter +=1;
+  //       winCounter();
+  //
+  //       setTimeout(function () {
+  //         $("#update-container").html("<h3>Player " + (ticTacToe.player + 1) + " is the winner!</h3>");
+  //       }, 400);
+  //       setTimeout(reset, 4000);
+  //       scoreboardUpdate();
+  //
+  //     } else if (ticTacToe.turnsCounter === 9){
+  //       ticTacToe.gamesCounter +=1;
+  //       setTimeout(function() {
+  //         $("#update-container").html("<h3>Game Over! It's a draw. Play again?</h3>");
+  //       }, 400);
+  //       setTimeout(reset, 4000);
+  //
+  //     } else {
+  //       ticTacToe.swapTurns();
+  //       //AI makes a move
+  //       //1. check for human move and available spots
+  //       //2. Rank available spots based on points
+  //       //3. Select highest valued spot
+  //     };
+  //
+  //   } else {
+  //     $("#update-container").html("<h3>Spot taken. Pick another spot on the board.</h3>")
+  //   };
+  // }
 
-  var winConfetti = function () {
+  // var points = [
+  //   ["5"],
+  //   ["1", "3", "7", "9"],
+  //   ["2", "4", "6", "8"]
+  // ];
+  // // threePoints = [5];
+  // // twoPoints = [1, 3, 7, 9];
+  // // onePoint = [2, 4, 6, 8];
+  //
+  // var selectSquare = function() {
+  //   var board = ticTacToe.board;
+  //
+  //   if (ticTacToe.turnsCounter === 1 && board[1][1] === null) {
+  //     addToken($("#5"), computerToken);
+  //   }
+  //
+  //   if (board[1][1] != null) {
+  //
+  //
+  //   }
+  // }
+  //
+  // var $availSquares = $(".square").not(".token-one, .token-two, .token-three, .token-four").map(function   (){
+  //     return this.id;
+  //   });
+  //
+  // var pointCheck = function() {
+  //   for (var i = 0; i < points.length; i++) {
+  //     for (var j = 0; j < points[i].length; j++) {
+  //       if (points[i][j] === $availSquares[i]){
+  //         console.log($availSquares, $availSquares[i]);
+  //         return true
+  //
+  //       }
+  //     }
+  //   }
+  // };
 
-    var $canvas = $("#canvas");
-    var ctx = $canvas.getContext("2d");
-
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-
-    $canvas.width = width;
-    $canvas.height = height;
-
-    var max = 100; //max number of confetti
-    var confetti = [];
-
-    var random = function(number) {
-      return Math.floor(Math.random() * number)
-    }
-
-    for (var i = 0; i < max; i++) {
-      confetti.push({
-        color: "rgba( " + random(255) + ", " + random(255) + ", " + random(255) + ", " + random(1) + " )",
-        x: random(width),
-        y: random(height),
-        
-
-
-      })
-    }
-
-  }
+// var winConfetti = {
+//     max: 100, //max number of confetti
+//     confettis: [],
+//
+//     random: function(number) {
+//       return Math.floor(Math.random() * number)
+//     },
+//
+//     makeConfetti: function() {
+//
+//       if (winConfetti.confettis.length < max) {
+//         var confetti = $("<div class='confetti'></div>");
+//         confetti.css ({
+//           background: "rgba( " + random(255) + ", " + random(255) + ", " + random(255) + ", " + random(1) + " )",
+//           top: random(window.innerHeight),
+//           left: random(window.innerWidth),
+//           position: "absolute"
+//         });
+//         winConfetti.confettis.push(confetti);
+//         $("body").append(confetti);
+//         return confetti;
+//       }
+//       else //update else statement to clearInterval
+//     },
+//
+//     moveConfetti: function
+//   }
 
 });
